8989691bb6e6f949eded1bc331c72eae81226e51
Marc Hanheide <marc@hanheide.net>
2018-10-30 22:23:53 +0000

    Trimmed the branch to only the walking_group_recovery sub directory

--- strands_monitored_nav_states/src/strands_monitored_nav_states/__init__.py
+++
@@ -1,7 +0,0 @@
-import monitor_bumper
-import recover_bumper
-import monitor_stuck_on_carpet
-import recover_stuck_on_carpet
-import recover_nav
-
-__all__ = ['monitor_bumper',  'recover_bumper', 'monitor_stuck_on_carpet', 'recover_stuck_on_carpet', 'recover_nav']
